56c9e96a9ef20431040ef85449efa79f920d7966
LyzzCL <LyzzCL@proton.me>
2024-04-18 21:31:07 -0600

    Some margins and paddings fixed, active section indicator removed

--- main.js
+++ main.js
@@ -2,42 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const prevBtn = document.getElementById("btnPrevious");
   const nextBtn = document.getElementById("btnNext");
   const container = document.querySelector("main > div");
-  const indicators = document.querySelectorAll("main > ul > li");
-
-  let currentIndex = 0;
-
-  function updateIndicator() {
-    indicators.forEach((indicator, index) => {
-      if (index === currentIndex) {
-        indicator.style.color = "var(--primary-color)";
-      } else {
-        indicator.style.color = "var(--primary-warm-color)";
-      }
-    });
-  }
-
   // For scroll navigation
   prevBtn.addEventListener("click", function () {
     container.scrollBy(-container.offsetWidth, 0);
-    currentIndex = Math.max(currentIndex - 1, 0);
-    updateIndicator();
   });
 
   nextBtn.addEventListener("click", function () {
     container.scrollBy(container.offsetWidth, 0);
-    currentIndex = Math.min(currentIndex + 1, indicators.length - 1);
-    updateIndicator();
   });
 
-  // Video Loading
-  const video = document.getElementById('bgVideo');
+  // Video Loading on desktop
+  const video = document.getElementById("bgVideo");
 
   const screenWidth = window.innerWidth;
-  const isDesktop = screenWidth>=768;
+  const isDesktop = screenWidth >= 768;
 
   if (isDesktop) {
     video.querySelector("source").src = "./assets/vids/bg.webm";
     video.load();
   }
-
 });
